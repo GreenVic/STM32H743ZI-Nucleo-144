5fcd37682b714a575ea5d130537d55bb30838ddc
David R. Piegdon <dgit@piegdon.de>
2020-08-05 19:16:57 +0000

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+
+Example STM32H743ZI firmware setup
+==================================
+
+This repo contains everything needed to compile firmware for an STM32H743ZI Nucleo 144 board
+using standard tools like GCC, GDB and Make.
+
+BlackMagicProbe or JLINK debugger probes are expected to flash the firmware,
+but make-targets to do that are provided.
---
+++ src/main.c
@@ -0,0 +1,26 @@
+
+#include "stm32h7xx_nucleo.h"
+
+int main(void)
+{
+	BSP_LED_Init(LED1);
+	BSP_LED_Init(LED2);
+	BSP_LED_Init(LED3);
+
+	for(;;)
+	{
+		BSP_LED_Toggle(LED1);
+		for (int j = 0; j < 100000; j++)
+			;
+
+		BSP_LED_Toggle(LED2);
+		for (int j = 0; j < 100000; j++)
+			;
+
+		BSP_LED_Toggle(LED3);
+		for (int j = 0; j < 100000; j++)
+			;
+	}
+
+	return 0;
+}
